97599982fee2245daeaa58862ee199f815eadccf
Manas <harlequinsguitar@gmail.com>
2013-07-26 19:22:07 +0530

    tables and custom css libraries added.

---
+++ tables.sql
@@ -0,0 +1,17 @@
+create database answerMe;
+
+use answerMe;
+
+create table questions(
+	qno int primary key,
+	qtext varchar(100) not null,
+	nchoices int not null,
+	answer int
+);
+
+create table choices(
+	qno int,
+	cno int,
+	foreign key(qno) references questions(qno),
+	choice varchar(100) not null
+);
