532b0226f8ebc43632e8d5563b0a555aeedf6cdf
Manas <harlequinsguitar@gmail.com>
2013-09-12 19:18:14 +0530

    form validation

--- js/createtest.js
+++ js/createtest.js
@@ -1,15 +1,31 @@
 $(document).ready(function(){
 	var reqTest = new AjaxReq();
 	$("#done").bind('click',function(){
-		var data = "";
-		data += $("#tname").val();
-		data += ';';
-		data += $("#noq").val();
-		data += ';';
-		data += $("#negyes").is(':checked') ? 'on' : 'off';
-		data += ';';
-		data += $("#time").val();
-		data += ';';
-		reqTest.send(data);
+		if ($('#noq').val() == '') {
+			alert('No of questions field cannot be empty');
+		} else if ($('#tname').val() == ''){
+			alert('Test Name field cannot be empty');
+		}else if ($('#time').val() == '') {
+			alert('Time field cannot be empty');
+		} else{
+			if (isNaN($('#noq').val())) {
+				alert('No of questions is not a valid number');
+			} else{
+				if (isNaN($('#time').val())) {
+					alert('Time specified is not a valid number');
+				} else{
+					var data = "";
+					data += $("#tname").val();
+					data += ';';
+					data += $("#noq").val();
+					data += ';';
+					data += $("#negyes").is(':checked') ? 'on' : 'off';
+					data += ';';
+					data += $("#time").val();
+					data += ';';
+					reqTest.send(data);
+				}
+			}
+		}
 	});
 });
