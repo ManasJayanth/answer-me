f8a4c4f30b56b2fd2e6dd1529843048c76049582
Manas <harlequinsguitar@gmail.com>
2013-09-19 21:10:35 +0530

    Commented code removed.

--- php/ajax.php
+++ php/ajax.php
@@ -1,33 +1,5 @@
 <?php
 define('idlimit', 6);
-// function fetchQuestions() {
-// 	$database = "answerMe";  // the name of the database.
-// 	$server = "127.0.0.1";  // server to connect to.
-// 	$db_user = "root";  // mysql username to access the database with.
-// 	$db_pass = "mysqlmypass";  // mysql password to access the database with.
-// 	$table = "questions";
-// 	$result = array();
-
-// 	try {
-// 	  $testh = new PDO ("mysql:host=$server;dbname=$database", $db_user, $db_pass);
-// 	  $testh->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
-// 	  $testh->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
-// 	  $sql = "select COUNT(*) from questions";
-// 	  $pstatement = $testh->prepare($sql);
-// 	  $success = $pstatement->execute();
-// 	  $num = $pstatement->fetchColumn();
-// 	  $sql = "select * from $table";
-// 	  $pstatement = $testh->prepare($sql);
-// 	  $success = $pstatement->execute();
-// 	  $result = $pstatement->fetchAll(PDO::FETCH_COLUMN, 1);
-// 	  return $result;
-// 	}
-	// catch(PDOException $e)
-	// {
-	//   $result[0] = "Following error was encountered <br />";
-	//   return $result;
-	// }
-// }
 
 function createTest($data) {
 	$tname = $data[0];
