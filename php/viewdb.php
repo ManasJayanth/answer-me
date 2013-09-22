e4d6e9d46f32317853d9416fdc0cfcda82a715f2
Manas <harlequinsguitar@gmail.com>
2013-09-22 14:53:58 +0530

    Test code removed.

--- php/viewdb.php
+++ php/viewdb.php
@@ -39,27 +39,6 @@ $result = $pstatement->fetchAll();
             </div>
         </div>
         <div id="details">
-
-
-<button data-toggle="modal" href="#myModal" type="button" class="btn btn-primary btn-lg">Link</button>
- 
-<!-- Modal -->
-<div id="myModal" class="modal hide fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-<div class="modal-header">
-<button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
-<h3 id="myModalLabel">Modal header</h3>
-</div>
-<div class="modal-body">
-<p>One fine body…</p>
-</div>
-<div class="modal-footer">
-<button class="btn" data-dismiss="modal" aria-hidden="true">Close</button>
-<button class="btn btn-primary">Save changes</button>
-</div>
-</div>
-
-
-
         <table class="table table-bordered table-striped table-condensed table-hover">
             <caption> </caption>
             <thead>
