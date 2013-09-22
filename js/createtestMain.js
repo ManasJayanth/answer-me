e14afd430f980ca18f4e5cae27ecd0ea0babf5f1
Manas <harlequinsguitar@gmail.com>
2013-09-22 15:01:37 +0530

    createtestMain.js added.

---
+++ js/createtestMain.js
@@ -0,0 +1,25 @@
+require.config({
+	'paths': {
+		'jquery': 'lib/jquery',
+		'bootstrap': 'lib/bootstrap',
+		'ajaxcreatetest': 'ajax',
+		'createtest': 'createtest'
+	},
+	shim: {
+		'bootstrap': {
+			deps: ['jquery'],
+		},
+		'ajaxcreatetest': {
+			deps: ['jquery'],
+		},
+		'createtest': {
+			deps: ['jquery'],
+		}
+	}
+});
+
+require([
+    'jquery', 'bootstrap','ajaxcreatetest','createtest'
+], function(jquery,bootstrap) {
+    console.log('createtest.html js dependencies loaded');
+})
